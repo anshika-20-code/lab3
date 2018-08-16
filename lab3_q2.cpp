348256a217180c2dc254cb4b616a2ab62ce613f5
anshika-20 <42172944+anshika-20@users.noreply.github.com>
2018-08-16 15:44:25 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,23 @@
+//include library
+#include <iostream>
+using namespace std;
+
+//main function
+int main()
+{
+int a,b,sum,substract,product,division,remainder ; //declaration
+a = 5;
+b = 2;
+sum = a + b; // adds 2 numbers
+substract = a - b; //claculates the difference
+product; a * b; //calculates the difference
+division; a / b; //divides the numbers
+remainder; a % b;
+cout << "numbers are"<< a << b << endl;
+cout << "results in in" << a << b << endl;
+cout << "sum is" << sum << endl;
+cout << "difference" << substract << endl;
+cout << "product is" << product << endl;
+cout << "quotient is" << division << endl;
+return 0;
+}
